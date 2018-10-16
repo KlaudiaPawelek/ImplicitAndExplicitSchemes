d541d8b0d3ab724d0214ebeb92d8fe4a106c9dac
SwagDePoule <e.p.combourieu@cranfield.ac.uk>
2018-10-16 18:06:17 +0100

    abc

--- ImplicitAndExplicitScheme/ImplicitAndExplicitScheme/main.cpp
+++ ImplicitAndExplicitScheme/ImplicitAndExplicitScheme/main.cpp
@@ -1,4 +1,24 @@
 <<<<<<< HEAD
 azert
 =======
+#include<iostream>
+#include "matrix.h" //class from C++ lab
+#include "vector.h" //class from C++ lab
+#include "Scheme.h" //mother class
+#include "ExplicitScheme.h" //inherited class
+#include "ImplicitScheme.h" //inherited class
+
+using namespace std;
+
+int main()
+{
+	// this is only test! you can delete it
+	ImplicitScheme iS;
+	iS.ComputeSizeOfMatrix(0.2, 10, 1, 0.01, 0.4);
+	cout << "Size of matrix: \n" << " -columns: " << iS.GetMatrix().getNcols() << "\n -rows: " << iS.GetMatrix().getNrows() << endl;
+	//end test
+
+	system("pause");
+	return 0;
+}
 >>>>>>> master
